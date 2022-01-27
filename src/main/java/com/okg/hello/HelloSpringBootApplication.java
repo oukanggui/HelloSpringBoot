f0df2c2e3a9419887313e1e0657d448c7f376cdb
oukanggui <13802885143@139.com>
2022-01-27 14:41:17 +0800

    添加quartz定时任务组件

--- src/main/java/com/okg/hello/HelloSpringBootApplication.java
+++ src/main/java/com/okg/hello/HelloSpringBootApplication.java
@@ -3,9 +3,11 @@ package com.okg.hello;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.okg.hello.dao.mapper")//使用MapperScan批量扫描所有的Mapper接口
+@EnableScheduling //启动时即启动定时任务
 public class HelloSpringBootApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/okg/hello/config/QuartzConfig.java
@@ -0,0 +1,43 @@
+package com.okg.hello.config;
+
+import com.okg.hello.task.DemoTask;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Quartz定时组件配置类，主要用于定于任务详情和任务触发器
+ */
+@Configuration
+public class QuartzConfig {
+    private static final String DEMO_TASK_IDENTITY = "demoTaskQuartz";
+
+    /**
+     * 定义任务详情
+     *
+     * @return
+     */
+    @Bean
+    public JobDetail quartzDetail() {
+        //指定job的名称和持久化保存任务
+        return JobBuilder.newJob(DemoTask.class).withIdentity(DEMO_TASK_IDENTITY).storeDurably().build();
+    }
+
+    /**
+     * 定义触发器
+     *
+     * @return
+     */
+    @Bean
+    public Trigger quartzTrigger() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+                .withIntervalInSeconds(15)   // 设置时间间隔，单位为s
+//                .withIntervalInHours(2)      // 两小时执行一次
+                .repeatForever();            // 无限重复
+        return TriggerBuilder.newTrigger().forJob(quartzDetail())// 任务详情
+                .withIdentity(DEMO_TASK_IDENTITY)
+                .withSchedule(scheduleBuilder)//添加定时调度器时钟
+                .build();
+    }
+
+}
