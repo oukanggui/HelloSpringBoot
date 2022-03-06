6c8f87b130ccb495bef3cd21348aca80d1663c54
oukanggui <13802885143@139.com>
2022-03-06 22:02:11 +0800

    注释优化

--- src/main/java/com/okg/hello/HelloSpringBootApplication.java
+++ src/main/java/com/okg/hello/HelloSpringBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@MapperScan("com.okg.hello.dao.mapper")//使用MapperScan批量扫描所有的Mapper接口
+@MapperScan("com.okg.hello.dao.mapper")//使用MapperScan批量扫描所有的Mapper接口进行映射后放到Spring容器里面
 @EnableScheduling //启动时即启动定时任务
 public class HelloSpringBootApplication {
 
