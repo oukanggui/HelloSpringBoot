447e15d3b280864725de6bba7912254ba8b745ef
oukanggui <13802885143@139.com>
2022-04-13 09:24:58 +0800

    采用yaml格式文件配置

--- src/main/java/com/okg/hello/config/InterceptorConfig.java
+++ src/main/java/com/okg/hello/config/InterceptorConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * Date: 2022-03-06
  * Describe:拦截器配置类
  * 需要实现WebMvcConfigurer类，重写相关方法进行拦截器注册及对应的规则配置
- * 通过@Configuration注释表明这个类是一个配置类
+ * 通过@Configuration注释表明这个类是一个配置类，方法通过@Bean注视返回配置类的Bean对象，其生命周期由Spring管理，默认为全局单例
  */
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
@@ -25,6 +25,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册拦截器，并匹配该拦截器使用的路径，即哪些接口开启这个拦截器：此处即是在删除用户时进行拦截校验
         registry.addInterceptor(userInterceptor())
+//                .addPathPatterns("/**") // 拦截所有请求
                 .addPathPatterns("/delete");
     }
 }
