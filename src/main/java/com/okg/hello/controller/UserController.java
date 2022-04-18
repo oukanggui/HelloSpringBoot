ed3ba62ee3a5dd536ad76fb2dca1ea22885f8c6f
oukanggui <13802885143@139.com>
2022-04-18 09:13:25 +0800

    注释优化

--- src/main/java/com/okg/hello/controller/UserController.java
+++ src/main/java/com/okg/hello/controller/UserController.java
@@ -23,6 +23,7 @@ import javax.servlet.http.HttpServletRequest;
  * DO（Data Object）：数据对象，与数据库表结构一一对应，通过DAO层向上传输的数据源对象
  * DTO（Data Transfer Object）：数据传输对象，Service/Controller层向外传输的对象
  * BO（Business Object）：业务对象，由Service层输出的封装业务逻辑的对象
+ * VO（View Object）：显示层对象，通常是Web向模板渲染引擎层传输的对象，也就是client端传给Controller的对象
  */
 @Slf4j
 @RestController // @ResponseBody+@Controller的结合注解
